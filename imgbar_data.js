3591fe042c29bcd4819eaa6dd5d5343937ef81b1
shenzizi <esperanzayaya22@gmail.com>
2016-12-06 19:14:27 +0800

    first commit

---
+++ imgbar_data.js
@@ -0,0 +1,116 @@
+var jo_data = [{
+
+    "food1": [{
+        "rank": [
+            [1, 6],
+            [2, 10],
+            [3, 11],
+            [4, 0],
+            [5, 0]
+        ],
+        "total": 51,
+        "img": "thumb-up.svg",
+        "name": "multi"
+    }, {
+        "rank": [
+            [1, 1],
+            [2, 3],
+            [3, 4],
+            [4, 17],
+            [5, 10]
+        ],
+        "total": 35,
+        "img": "stopwatch.svg",
+        "name": "waiting_time"
+    }, {
+        "rank": [
+            [1, 1],
+            [2, 2],
+            [3, 8],
+            [4, 13],
+            [5, 11]
+        ],
+        "total": 35,
+        "img": "drink.svg",
+        "name": "cp"
+    }, {
+        "rank": [
+            [1, 1],
+            [2, 3],
+            [3, 5],
+            [4, 7],
+            [5, 9]
+        ],
+        "total": 34,
+        "img": "money-bag.svg",
+        "name": "info"
+    }, {
+        "rank": [
+            [1, 2],
+            [2, 3],
+            [3, 4],
+            [4, 8],
+            [5, 9]
+        ],
+        "total": 34,
+        "img": "money-bag.svg",
+        "name": "info"
+    }]
+}, {
+    "food2": [{
+        "rank": [
+            [1, 1],
+            [2, 1],
+            [3, 0],
+            [4, 0],
+            [5, 0]
+        ],
+        "total": 51,
+        "img": "thumb-up.svg",
+        "name": "multi"
+    }, {
+        "rank": [
+            [1, 1],
+            [2, 0],
+            [3, 0],
+            [4, 0],
+            [5, 0]
+        ],
+        "total": 35,
+        "img": "stopwatch.svg",
+        "name": "waiting_time"
+    }, {
+        "rank": [
+            [1, 1],
+            [2, 2],
+            [3, 8],
+            [4, 13],
+            [5, 11]
+        ],
+        "total": 35,
+        "img": "drink.svg",
+        "name": "cp"
+    }, {
+        "rank": [
+            [1, 1],
+            [2, 3],
+            [3, 0],
+            [4, 0],
+            [5, 0]
+        ],
+        "total": 34,
+        "img": "money-bag.svg",
+        "name": "info"
+    }, {
+        "rank": [
+            [1, 2],
+            [2, 0],
+            [3, 0],
+            [4, 0],
+            [5, 0]
+        ],
+        "total": 34,
+        "img": "money-bag.svg",
+        "name": "info"
+    }]
+}]
---
+++ radar_data.js
@@ -0,0 +1,31 @@
+      var radar_data = [{
+          "label": "環境",
+          "metric": "a",
+          "domain": [0, 5]
+      }, {
+          "label": "美味",
+          "metric": "b",
+          "domain": [0, 5]
+      }, {
+          "label": "價格",
+          "metric": "c",
+          "domain": [0, 5]
+      }]
+
+      var rndData = [{
+          "label": "food",
+          "id": "food1",
+          "rate": {
+              "a": 3.50,
+              "b": 2.75,
+              "c": 4.00,
+          }
+      }, {
+          "label": "food",
+          "id": "food2",
+          "rate": {
+              "a": 1.50,
+              "b": 1.75,
+              "c": 1.00,
+          }
+      }];
