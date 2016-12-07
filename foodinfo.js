8ff51a8b94811ccd456a2e7b8d826d48c48ada2b
shenzizi <esperanzayaya22@gmail.com>
2016-12-07 22:56:22 +0800

    add imgs

---
+++ foodinfo.js
@@ -0,0 +1,42 @@
+var foodinfo = {
+    "results": [{
+        "id": "food1",
+        "name": "鳥人拉麵",
+        "lat": "25.044211",
+        "lng": "121.544465 ",
+        "hr":"一～日 11:30-22:00",
+        "phone":"02-2778-9866",
+        "price": "NT180~360",
+        "address":"106台北市大安區復興南路一段107巷5弄9號",
+        "imgenv":"food1_env.jpg",
+        "imgbest1":"food1_01.jpg",
+        "imgbest2":"food1_02.jpg",
+        "imgmenu":"food1_menu.jpg",
+        "icon":"noodles.svg",
+        "booking":"可訂位",
+        "type":"日式/拉麵",
+        "cp": 7,
+        "link1":"http://nikitarh.pixnet.net/blog/post/5588833-%E3%80%90%E9%A3%9F%E3%80%91%E3%80%90%E5%8F%B0%E5%8C%97%E3%80%91%E9%B3%A5%E4%BA%BA%E6%8B%89%E9%BA%B5totto-ramen%EF%BC%8E%E7%B4%90%E7%B4%84%E7%9A%84%E5%BF%85",
+        "link2":"http://www.meishijournal.com/2016/03/tottoramen.html"
+        
+    }, {
+        "id": "food2",
+        "name": "天海不解釋",
+        "lat": "25.030879",
+        "lng": "121.551094",
+        "hr":"一～日 11:30-14:30;17:00-21:00",
+        "phone":"02-2325-9566",
+        "price": "NT180~560",
+        "address":"106台北市大安區敦化南路二段63巷38號",
+        "imgenv":"food2_env.jpg",
+        "imgbest1":"food2_01.jpg",
+        "imgbest2":"food2_02.jpg",
+        "imgmenu":"food2_menu.jpg",
+        "icon":"sushi.svg",
+        "booking":"可訂位",
+        "type":"日式/生魚片",
+        "cp": 8,
+        "link1":"http://nixojov.pixnet.net/blog/post/300974558-%E3%80%90%E5%8F%B0%E5%8C%97%E9%A4%90%E5%BB%B3%E3%80%91%E5%A4%A9%E6%B5%B7%E4%B8%8D%E8%A7%A3%E9%87%8B%E6%97%A5%E5%BC%8F%E6%96%99%E7%90%86-%E6%96%B0%E9%AE%AE%E6%B5%B7%E5%91%B3",
+        "link2":"http://may1215may.pixnet.net/blog/post/367608282-(%E5%8F%B0%E5%8C%97%E9%A3%9F%E8%A8%98)%E5%A4%A9%E6%B5%B7-%E4%B8%8D%E8%A7%A3%E9%87%8B%E2%9D%A4%E6%97%A5%E5%BC%8F%E6%96%99%E7%90%86%E2%9D%A4%E6%8D%B7%E9%81%8B%E4%BF%A1%E7%BE%A9"
+    }]
+}
